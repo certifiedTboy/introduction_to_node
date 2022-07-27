ae6f70636550271a2a38a96deb6d4bf2f05faacc
certifiedTboy <79862233+certifiedTboy@users.noreply.github.com>
2022-07-27 14:51:11 +0100

    update app

--- src/app.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const express = require("express");
-const app = express();
-const basicRoutes = require("./routes/routes");
-
-app.use("/", basicRoutes);
-
-module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,47 @@
 const http = require("http");
-const app = require("./app");
-const server = http.createServer(app);
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const homeFile = fs.readFile("./public/index.html", null, (error, data) => {
+      if (error) {
+        res.writeHead(404);
+        res.write("Whoops! File not found!");
+      } else {
+        res.write(data);
+      }
+      res.end();
+    });
+  } else if (req.url === "/contact" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("./public/contact.html", null, (error, data) => {
+      if (error) {
+        res.writeHead(404);
+        res.write("Whoops! File not found!");
+      } else {
+        res.write(data);
+      }
+      res.end();
+    });
+  } else if (req.url === "/about" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("./public/about.html", null, (error, data) => {
+      if (error) {
+        res.writeHead(404);
+        res.write("Whoops! File not found!");
+      } else {
+        res.write(data);
+      }
+      res.end();
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "No page found" }));
+  }
+});
 
 const PORT = 3000;
 
-const startServer = async () => {
-  server.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}...`);
-  });
-};
-
-startServer();
+server.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}...`);
+});
